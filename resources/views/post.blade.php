d6377208f0095553186e150fd4323a77a682ae9a
bintikholifah16 <bintikholifah1616@gmail.com>
2024-06-13 14:59:06 +0700

    Dashboard Depan

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,6 +4,8 @@
 <article>
     <h1 class="mb-5">{{ $post->title }}</h1>
     
+    <p>By. Binti Kholifah in {{ $post->category->name }}</p>
+
     {!! $post->body !!}
 
     <a href="/posts">Back to Posts</a>
