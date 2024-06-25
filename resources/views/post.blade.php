775dfa2103a9d98d21610b1125d9879966794e72
bintikholifah16 <bintikholifah1616@gmail.com>
2024-06-25 14:07:56 +0700

    Seeder

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,9 +4,12 @@
 <article>
     <h1 class="mb-5">{{ $post->title }}</h1>
     
-    <p>By. Binti Kholifah in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>
+        By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> 
+        in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a>
+    </p>
 
     {!! $post->body !!}
 
-    <a href="/posts">Back to Posts</a>
+    <a href="/posts" class="d-block mt-2">Back to Posts</a>
 @endsection
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,22 @@
 @extends('layouts/main')
 
 @section('container')
-<article class="mb-5">
+<h1 class="mb-5">Halaman Blog Posts</h1>
+
     @foreach ($posts as $post)
+    <article class="mb-4 border-bottom pb-2">
         <h2>
-            <a href="/posts/{{ $post->slug }}">{{ $post->title}}</a>
+            <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title}}</a>
         </h2>
+
+        <p>
+            By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> 
+            in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a>
+        <p>
+
         <p>{{ $post->excerpt }}</p>
-      
+
+        <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more...</a>
+    </article>
     @endforeach
-</article>
 @endsection
